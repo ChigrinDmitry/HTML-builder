2bfc4337098b91aa83769fefed110702d874e06a
Dmitry Chigrin <chigrin.dmitry@gmail.com>
2022-05-23 23:59:39 +0500

    feat: add "04-copy-directoryr" task solution

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,20 @@
+const path = require('path');
+const fs = require('fs');
 
+fs.rm(path.join(__dirname, 'files-copy'), { recursive: true, force: true }, () => {
+  fs.mkdir(path.join(__dirname, 'files-copy'), () => {
+    fs.readdir(path.join(__dirname, 'files'), (err, elements) => {
+      
+    if (err) throw err;
+
+    elements.forEach(file => {
+        const readContent = fs.createReadStream(__dirname + `/files/${file}`);
+        const writeContent = fs.createWriteStream(__dirname + `/files-copy/${file}`);
+
+        readContent.on('data', (data) => {
+            writeContent.write(data);
+        });
+      });
+    });
+  });
+});
